4c3aa0198da0729e27b622efe95a55b184d7de9a
dmck <dmck@users.noreply.github.com>
2016-01-29 10:37:36 -0500

    Refactor and add listing-chooser toggle

--- script.js
+++ script.js
@@ -5,32 +5,69 @@
  *  Shows a "Hide Sidebar" or "Show Sidebar" to hide/show reddit's sidebar.
  */
 
-var showText = "Show Sidebar"
-var hideText = "Hide Sidebar"
+var showSidebarText = "Show Sidebar"
+var hideSidebarText = "Hide Sidebar"
+var showListingText = "Show Listing-Chooser"
+var hideListingText = "Hide Listing-Chooser"
 
+/* -- Show/Hide Sidebar --
+*  When 'hslink' (see below) is clicked do this: */
 $('body').on('click', 'a#hslink', function() {
+    /* Read whether the clicked-text says to hide */
     var text = $(this).text()
-    if(text == hideText) {
-        $(this).text(showText)
+    if(text == hideSidebarText) {
+    	/* 1) change text, 2) hide, 3) set status */
+        $(this).text(showSidebarText)
         $('div.side').hide()
-        localStorage['hideStatus'] = 'hide'
+        localStorage['sidebarStatus'] = 'hide'
     } else {
-        $(this).text(hideText)
+        $(this).text(hideSidebarText)
         $('div.side').show()
-        localStorage['hideStatus'] = 'show'
+        localStorage['sidebarStatus'] = 'show'
     }
     return false
 })
 
-var show = '<span id="hideSpan" class="showlink">'+
-    '<a id="hslink" href=""></a></span>';
+/* -- Show/Hide Listing-Chooser --
+*  When 'hlclink' (see below) is clicked do this: */
+$('body').on('click', 'a#hlclink', function() {
+	/* Read whether the clicked-text says to hide */
+    var text = $(this).text()
+    if(text == hideListingText) {
+    	/* 1) change text, 2) hide */
+        $(this).text(showListingText)
+        $(document.body).addClass('listing-chooser-collapsed');
+        /* listing-chooser status is notstored locally because
+        *  user can also click on the listing-chooser tray
+        *  status is checked below by seeing if 'body'
+        *  has the 'listing-chooser-collapsed' class.
+        */
+    } else {
+        $(this).text(hideListingText)
+        $(document.body).removeClass('listing-chooser-collapsed');
+    }
+    return false
+})
 
-$("div#header-bottom-right").append('<span class="separator">|</span>');
+
+/* Show Links */
+var show = '<span class="separator">|</span>'+
+	'<span id="hideSpan" class="showlink">'+
+    '<a id="hslink" href=""></a></span>'+
+    '<span class="separator">|</span>'+
+    '<a id="hlclink" href=""></a></span>';
+
+/* if sidebar status stored 'hide' then give option to 'show' */
 $("div#header-bottom-right").append(show);
-if (localStorage["hideStatus"] == "hide") {
-    $('div.side').hide()
-    $('#hslink').text(showText)
+if (localStorage["sidebarStatus"] == "hide") {
+    $('#hslink').text(showSidebarText)
 } else {
-    $('#hslink').text(hideText)
+    $('#hslink').text(hideSidebarText)
+}
+/* if listing-chooser is collapsed then give option to 'show' */
+if ($("body").hasClass('listing-chooser-collapsed')) {
+    $('#hlclink').text(showListingText)
+} else {
+    $('#hlclink').text(hideListingText)
 }
 
