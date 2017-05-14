00ad1d30392ceb4d7fcc46124be98beac60adeef
Hrishikesh Bakshi <bakshi.hrishikesh@gmail.com>
2017-05-14 18:44:42 -0400

    Better support for r/videos like css
    Some subreddits used css that made the hiding not work properly

--- script.js
+++ script.js
@@ -7,17 +7,20 @@
 
 var showText = "Show Sidebar"
 var hideText = "Hide Sidebar"
+var cssId    = "sidebarhider"
 
 $('body').on('click', 'a#hslink', function() {
     var text = $(this).text()
     if(text == hideText) {
         $(this).text(showText)
         $('div.side').hide()
+        hideUsingCss()
         localStorage['hideStatus'] = 'hide'
     } else {
         $(this).text(hideText)
         $('div.side').show()
         localStorage['hideStatus'] = 'show'
+        showUsingCss()
     }
     return false
 })
@@ -29,8 +32,23 @@ $("div#header-bottom-right").append('<span class="separator">|</span>');
 $("div#header-bottom-right").append(show);
 if (localStorage["hideStatus"] == "hide") {
     $('div.side').hide()
+    hideUsingCss()
     $('#hslink').text(showText)
 } else {
+    showUsingCss()
     $('#hslink').text(hideText)
+}
+
+function hideUsingCss() {
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = "body > div.content {margin-right:5px;}";
+    css.id = cssId;
+    document.body.appendChild(css);
+}
+
+function showUsingCss() {
+    var cssNode = document.getElementById(cssId);
+    cssNode && cssNode.parentNode.removeChild(cssNode)
 }
 
